776e86101d081095f5bb8f9652a40a93c6621849
damiannelus <damiannelus@gmail.com>
2026-02-11 20:10:44 +0100

    fix: update revision number for index.html in service worker

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-ca84f546'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.vlgi0aodbbs"
+    "revision": "0.kr54t56bvtg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
